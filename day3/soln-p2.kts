cd9ec2112997ed3812b185973d6ae1337bcda864
Duana Saskia <starkcoffee@users.noreply.github.com>
2020-12-06 06:54:50 +1000

    day3: huge performance improvement 😅

--- day3/soln-p2.kts
+++ day3/soln-p2.kts
@@ -6,17 +6,9 @@ fun treeCount(lines: List<String>, step_across:Int, step_down:Int): Int {
   val num_rows = lines.size
   val num_cols = lines.first().length
 
-  val total_dots_across = 1 + step_across*(num_rows/step_down-1)
-  val repeats_needed = total_dots_across / num_cols
-  println(repeats_needed)
-
-  val forestMap = lines.map{ line ->
-    line.repeat(1 + repeats_needed) // it counts the original string as a "repeat" 
-  }
-
   val coords = (step_down..num_rows-1 step step_down).map{ row -> Pair(row/step_down*step_across, row) }
 
-  val path = coords.map{(col,row) -> forestMap[row][col]}
+  val path = coords.map{(col,row) -> lines[row][col%num_cols]}
   val numTrees = path.count{ it == '#' }
 
   return numTrees
