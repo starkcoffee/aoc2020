c0d5b5f0908157b2d3c65265b23c15faf5ebfa61
Duana Saskia <starkcoffee@users.noreply.github.com>
2020-12-05 10:23:10 +1000

    day1: readability++

--- day1/soln-p2.kts
+++ day1/soln-p2.kts
@@ -1,18 +1,18 @@
 import java.io.File
 
 
-fun findPairWithSum(targetSum: Int, list: List<Int>): Pair<Int, Int>? {
+fun findPairWithSum(targetSum: Int, sortedList: List<Int>): Pair<Int, Int>? {
+
+  if (sortedList.isEmpty()) return null
 
   var lower = 0
-  var higher = list.count() - 1
-
-  if (list.isEmpty()) return null
+  var higher = sortedList.count() - 1
 
   do {
-    val sum = list[lower] + list[higher]
+    val sum = sortedList[lower] + sortedList[higher]
 
     if (sum == targetSum) {
-      return Pair(list[lower], list[higher])
+      return Pair(sortedList[lower], sortedList[higher])
     }
 
     if (sum < targetSum) lower++ else higher-- 
@@ -22,10 +22,10 @@ fun findPairWithSum(targetSum: Int, list: List<Int>): Pair<Int, Int>? {
   return null
 }
 
-fun findTripleWithSum(targetSum: Int, list: List<Int>): Triple<Int, Int, Int>? {
+fun findTripleWithSum(targetSum: Int, sortedList: List<Int>): Triple<Int, Int, Int>? {
 
-  for ((index, number) in list.withIndex()) {
-    val pair = findPairWithSum(targetSum - number, list.slice(index+1..list.size-1))
+  for ((index, number) in sortedList.withIndex()) {
+    val pair = findPairWithSum(targetSum - number, sortedList.slice(index+1..sortedList.size-1))
     if (pair != null) {
       return Triple(number, pair.first, pair.second) 
     }
